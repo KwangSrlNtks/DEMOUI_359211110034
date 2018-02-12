1c6712774ce136d891324c5d5a54ee07c326e353
KwangSrlNtks <kwangsiriluk2722@gmail.com>
2018-02-12 16:51:08 +0700

    Login เกือบ OK

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,6 +19,12 @@ public class Controller implements Initializable {
     private loginModel loginModel = new loginModel();
     @FXML
     private Label dbStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private Button btnLogin;
 
 
     @Override
@@ -23,4 +37,36 @@ public class Controller implements Initializable {
         }
 
     }//initialize
+
+    @FXML
+    public void Login(ActionEvent event){
+
+        try {
+            if (this.loginModel.isLogin(this.username.getText(),
+                    this.password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+
+                adminLogin();
+
+            } else {
+                JOptionPane.showMessageDialog(null,"Your username or "+
+                "password is not corrected.");
+            }
+        }catch (Exception localException){
+            localException.printStackTrace();
+        }
+
+    }//Login
+
+    private void adminLogin() {
+        try {
+            Stage adminstage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = (Pane) adminLoader.load(getClass().getResource("/Admin/AdminDashBoard.fxml").openStream());
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
 }
